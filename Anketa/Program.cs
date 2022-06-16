de032025ca1498aab69406c8846b985b7ecec3dd
sTorov <sotorov2011@yandex.ru>
2022-06-16 23:35:33 +0300

    Добавлена фиксация кличек петомцев
    Если питомец есть, то происходит запрос количества петомцев. Также вызывается метод, принимающий на вход количество питомцев и возвращающий массив кличек (заполнение с клавиатуры);

--- Anketa/Program.cs
+++ Anketa/Program.cs
@@ -4,7 +4,7 @@ namespace Anketa
 {
     class Program
     {
-        static (string Name, string Surname, int Age, int PetCount) User()
+        static (string Name, string Surname, int Age, bool HavePet, int PetCount, string[] PetName) User()
         {
             Console.Write("Введите имя: ");
             string Name = Console.ReadLine();
@@ -15,15 +15,30 @@ namespace Anketa
             Console.Write("Введите возраст: ");
             int Age = int.Parse(Console.ReadLine());
 
-            int PetCount = 0;
+            byte PetCount = 0;
+            string[] PetName = new string[PetCount];
             Console.Write("Есть ли у Вас домашние животные 'да/нет': ");
             string str = Console.ReadLine();
             bool HavePet = str == "да" || str == "Да" ? true : false;
             if (HavePet == true)
             {
-
+                Console.Write("Количество петомцев: ");
+                PetCount = byte.Parse(Console.ReadLine());
+                PetName = Pet(PetCount);
             }
-            return (Name, Surname, Age, PetCount);
+            return (Name, Surname, Age, HavePet, PetCount, PetName);
+        }
+
+        static string[] Pet(byte PetCount)
+        {
+            string[] PetList = new string[PetCount];
+            Console.WriteLine("Введите имена петомцев:");
+            for (int i = 0; i < PetList.Length; i++)
+            {
+                Console.Write((i + 1) + ".");
+                PetList[i] = Console.ReadLine();
+            }
+            return PetList;
         }
 
         static void Main(string[] args)
