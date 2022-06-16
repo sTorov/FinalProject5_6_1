a9ce0ffd72fbeb2f6b91f37d0d27dea6664714ff
sTorov <sotorov2011@yandex.ru>
2022-06-16 23:07:22 +0300

    Добавлена проверка наличия петомцев в метод User()

--- Anketa/Program.cs
+++ Anketa/Program.cs
@@ -4,7 +4,7 @@ namespace Anketa
 {
     class Program
     {
-        static (string Name, string Surname, int Age) User()
+        static (string Name, string Surname, int Age, int PetCount) User()
         {
             Console.Write("Введите имя: ");
             string Name = Console.ReadLine();
@@ -15,7 +15,15 @@ namespace Anketa
             Console.Write("Введите возраст: ");
             int Age = int.Parse(Console.ReadLine());
 
-            return (Name, Surname, Age);
+            int PetCount = 0;
+            Console.Write("Есть ли у Вас домашние животные 'да/нет': ");
+            string str = Console.ReadLine();
+            bool HavePet = str == "да" || str == "Да" ? true : false;
+            if (HavePet == true)
+            {
+
+            }
+            return (Name, Surname, Age, PetCount);
         }
 
         static void Main(string[] args)
