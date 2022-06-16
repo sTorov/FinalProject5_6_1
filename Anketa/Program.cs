37029d05569eb4062d084fcb734b9ff6c26ff15c
sTorov <sotorov2011@yandex.ru>
2022-06-16 22:18:02 +0300

    Создан метод User()
    User() возвращает кортеж. В методе происходит заполнение данных с клавиатуры о пользователе. (Имя, Фамилия, Возраст)

--- Anketa/Program.cs
+++ Anketa/Program.cs
@@ -4,9 +4,25 @@ namespace Anketa
 {
     class Program
     {
+        static (string Name, string Surname, int Age) User()
+        {
+            Console.Write("Введите имя: ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Введите фамилию: ");
+            string Surname = Console.ReadLine();
+
+            Console.Write("Введите возраст: ");
+            int Age = int.Parse(Console.ReadLine());
+
+            return (Name, Surname, Age);
+        }
+
         static void Main(string[] args)
         {
+            var Person = User();
 
+            Console.ReadKey();
         }
     }
 }
